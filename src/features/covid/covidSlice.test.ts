9e6d7830c2a17789e40619d39c5b295aa5a8541d
rui-watanabe <wataru610806@gmail.com>
2021-05-02 15:35:54 +0900

    add covidSlice test when fetchAsyncData pending #7

--- src/features/covid/covidSlice.test.ts
+++ src/features/covid/covidSlice.test.ts
@@ -7,8 +7,23 @@ import reducer, {
 import initPrefectureData from './prefectureData.json';
 import payloadPrefectureData2 from './prefectureData2.json';
 
-describe('covidSliice extraReducers test', () => {
+describe('covidSlice extraReducers test', () => {
   const initialState: covidState = initPrefectureData.data;
+  it('Should output fetchAsyncData new state when pending', () => {
+    const action = {
+      type: fetchAsyncData.pending.type,
+      payload: {
+        date: payloadPrefectureData2.data.date,
+        data: payloadPrefectureData2.data.data,
+      },
+    };
+    const state = reducer(initialState, action);
+    expect(state.date).toEqual('2021年3月31日');
+    expect(state.isLoading).toEqual(true);
+    expect(state.data.hokkaido.hospitalize.normal).toEqual(0);
+    expect(state.data.hokkaido.outPatient.normal).toEqual(0);
+    expect(state.data.hokkaido.emergency.normal).toEqual(0);
+  });
   it('Should output fetchAsyncData new state when fulfilled', () => {
     const action = {
       type: fetchAsyncData.fulfilled.type,
@@ -18,7 +33,8 @@ describe('covidSliice extraReducers test', () => {
       },
     };
     const state = reducer(initialState, action);
-    expect(state.date).toEqual('2021年3月31日');
+    expect(state.date).toEqual('2021年4月1日');
+    expect(state.isLoading).toEqual(false);
     expect(state.data.hokkaido.hospitalize.normal).toEqual(1);
     expect(state.data.hokkaido.outPatient.normal).toEqual(2);
     expect(state.data.hokkaido.emergency.normal).toEqual(3);
