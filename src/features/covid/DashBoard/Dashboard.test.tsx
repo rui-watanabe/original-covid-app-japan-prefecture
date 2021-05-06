51a4392a4c9dd835683cc5541903620aca662531
rui-watanabe <wataru610806@gmail.com>
2021-05-06 23:25:16 +0900

    lintfix #7

--- src/features/covid/DashBoard/Dashboard.test.tsx
+++ src/features/covid/DashBoard/Dashboard.test.tsx
@@ -1,16 +1,20 @@
-import React from "react";
-import { render, screen, cleanup } from "@testing-library/react";
-import { covidState, selectCurrentPrefecture, selectData, selectLoading } from '../covidSlice';
-import DashBoard from "./DashBoard";
-import { useSelector, useDispatch } from 'react-redux'
+import React from 'react';
+import { render, screen, cleanup } from '@testing-library/react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  covidState,
+  selectCurrentPrefecture,
+  selectData,
+  selectLoading,
+} from '../covidSlice';
+import DashBoard from './DashBoard';
 import prefectureData from '../prefectureData.json';
-import { RootState } from "../../../app/store";
 
-const data: covidState = prefectureData.data;
+const { data } = prefectureData;
 
-jest.mock('react-redux')
-const useSelectorMock = useSelector as jest.Mock<covidState | boolean | string>
-const useDispatchMock = useDispatch as jest.Mock
+jest.mock('react-redux');
+const useSelectorMock = useSelector as jest.Mock<covidState | boolean | string>;
+const useDispatchMock = useDispatch as jest.Mock;
 
 afterEach(() => {
   cleanup();
@@ -18,32 +22,27 @@ afterEach(() => {
 });
 
 describe('DashBoard rendering test', () => {
-    beforeEach(() => {
-      useSelectorMock.mockImplementation((selector): any => 
-      {
-        if (selector === selectData) 
-        {
-          return data
-        }
-        else if(selector === selectCurrentPrefecture)
-        {
-          return "hokkaido"
-        }
-        else if(selector === selectLoading)
-        {
-          return false
-        }
-        else
-        {
-          return undefined
-        }
+  beforeEach(() => {
+    useSelectorMock.mockImplementation((selector):
+      | covidState
+      | string
+      | boolean => {
+      if (selector === selectData) {
+        return data;
       }
-    );
-    useDispatchMock.mockReturnValue(jest.fn())
-    })
-  it("Should render the elements correctly", async () => {
+      if (selector === selectCurrentPrefecture) {
+        return 'hokkaido';
+      }
+      if (selector === selectLoading) {
+        return false;
+      }
+      return '';
+    });
+    useDispatchMock.mockReturnValue(jest.fn());
+  });
+  it('Should render the elements correctly', async () => {
     render(<DashBoard />);
     screen.debug();
-    expect(await screen.findByText(/全国医療提供体制状況/)).toBeInTheDocument();//find~は結果が反映されるまでまつ
+    expect(await screen.findByText(/全国医療提供体制状況/)).toBeInTheDocument(); // find~は結果が反映されるまでまつ
   });
-})
+});
